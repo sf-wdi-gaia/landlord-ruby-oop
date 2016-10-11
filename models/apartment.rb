d7c904f40f17f9e936f298c6de3296a076348fa5
rhamill1 <rhamill11@gmail.com>
2016-10-10 19:09:23 -0700

    passes apartment_spec

--- models/apartment.rb
+++ models/apartment.rb
@@ -1,5 +1,20 @@
 class Apartment
-  def initialize()
+  attr_accessor :address, :monthly_rent, :sqft, :num_beds, :num_baths, :renters
+  def initialize(address, monthly_rent, sqft, num_beds, num_baths, renters)
     @address = address
+    @monthly_rent = monthly_rent
+    @sqft = sqft
+    @num_beds = num_beds
+    @num_baths = num_baths
+    @renters = renters
   end
+
+  def add_tenant(tenant)
+    @tenant = tenant
+    if @renters.length < @num_beds
+      @renters.push(@tenant)
+
+    end
+  end
+
 end
